55858c241829f394a49fe30f28de1646e240c0da
Samir Buch <samir.buch@temple.edu>
2023-01-25 12:43:22 -0500

    lab2: Initial commit

---
+++ lab2.c
@@ -0,0 +1,54 @@
+/* Coding Problems (25 points) */
+
+/*
+    Please write code for the two problems below.  Write the code for both
+    problems in the area below designated inside of the main function.
+    When completed, the main function should run the code for both problems.
+
+    Use floating point arithmetic and print values to two decimal places.
+    
+    1. Write code that calculates mileage reimbursement for a salesperson
+    at a rate of $0.54 per mile. Your program should get the mileage from
+    the user in this manner:
+
+    MILEAGE REIMBURSEMENT CALCULATOR
+    Enter beginning odometer reading: <user response>
+    Enter ending odometer reading: <user response>
+    You traveled <value> miles. At $0.48 per mile, your reimbursement $<value>.
+    
+    2. Write code to convert a temperature in degrees Fahrenheit to
+    degrees Celsius. The formula is C = (5/9)*(F - 32).  Your program should
+    get the temperature Fahrenheit from the user and print the result as:
+    
+    FAHRENHEIT TO CELSIUS CALCULATOR
+    Enter temperature Fahrenheit: <value>
+    <value> degrees Fahrenheit is <value> degrees Celsius
+*/
+
+// Preprocessor directives
+#include <stdio.h>
+#include <math.h> // Thanks https://stackoverflow.com/a/1344261zz
+
+// Main function
+int main(void) {
+
+}
+// End main function
+
+// Coding Problem 1
+int reimbursementCalculator(int miles) {
+
+}
+
+// Coding Problem 2
+int farenheitToCelsius(int farenheit) {
+
+}
+
+/*  
+    Submit a text file (use the file submission facility in the Canvas Lab
+    assignment) with the code for both parts entered as above.  Also, copy
+    and paste the output of a successful run as a comment below this one
+*/
+
+/* OUTPUT GOES BELOW */
