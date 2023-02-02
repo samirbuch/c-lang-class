05eaffc03cd3a25ef718a3f3ab658148e1108b75
Samir Buch <samir.buch@temple.edu>
2023-02-02 11:48:35 -0500

    lab2: reimbursement done

--- lab2.c
+++ lab2.c
@@ -6,7 +6,7 @@
     When completed, the main function should run the code for both problems.
 
     Use floating point arithmetic and print values to two decimal places.
-    
+
     1. Write code that calculates mileage reimbursement for a salesperson
     at a rate of $0.54 per mile. Your program should get the mileage from
     the user in this manner:
@@ -14,12 +14,12 @@
     MILEAGE REIMBURSEMENT CALCULATOR
     Enter beginning odometer reading: <user response>
     Enter ending odometer reading: <user response>
-    You traveled <value> miles. At $0.48 per mile, your reimbursement $<value>.
-    
+    You traveled <value> miles. At $0.54 per mile, your reimbursement $<value>.
+
     2. Write code to convert a temperature in degrees Fahrenheit to
     degrees Celsius. The formula is C = (5/9)*(F - 32).  Your program should
     get the temperature Fahrenheit from the user and print the result as:
-    
+
     FAHRENHEIT TO CELSIUS CALCULATOR
     Enter temperature Fahrenheit: <value>
     <value> degrees Fahrenheit is <value> degrees Celsius
@@ -27,25 +27,47 @@
 
 // Preprocessor directives
 #include <stdio.h>
-#include <math.h> // Thanks https://stackoverflow.com/a/1344261zz
-
-// Main function
-int main(void) {
-
-}
-// End main function
 
 // Coding Problem 1
 int reimbursementCalculator(int miles) {
-
+    return miles * 0.54;
 }
 
 // Coding Problem 2
 int farenheitToCelsius(int farenheit) {
-
+    return (farenheit - 32) * (5/9);
 }
 
-/*  
+// Main function
+int main(void) {
+
+    int beginningOdometer, endingOdometer;
+
+    printf("Enter beginning odometer reading: ");
+    scanf("%i", &beginningOdometer);
+
+    printf("Enter ending odometer reading: ");
+    scanf("%i", &endingOdometer);
+
+    if(beginningOdometer < 0 || endingOdometer < 0) {
+        printf("Your odometer rolled over. Try again.\n");
+        return 1;
+    }
+
+    int difference = endingOdometer - beginningOdometer;
+    if(difference <= 0) {
+        printf("You didn't go anywhere. Try again.\n");
+        return 1;
+    }
+
+    int reimbursement = reimbursementCalculator(difference);
+
+    printf("You traveled %i miles at a rate of $0.54 per mile. Your reimbursement is: $%i.\n\n", difference, reimbursement);
+
+}
+// End main function
+
+/*
     Submit a text file (use the file submission facility in the Canvas Lab
     assignment) with the code for both parts entered as above.  Also, copy
     and paste the output of a successful run as a comment below this one
